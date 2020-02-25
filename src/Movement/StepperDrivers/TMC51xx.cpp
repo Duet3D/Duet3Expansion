6d453a114a563428dfd42685634e932cf646ca20
David Crocker <dcrocker@eschertech.com>
2020-02-25 11:24:02 +0000

    Version 3.01-RC3 provisional
    Fixed TMC5160 driver (was always timing out because of change to
    TaskBase::Take)

--- src/Movement/StepperDrivers/TMC51xx.cpp
+++ src/Movement/StepperDrivers/TMC51xx.cpp
@@ -1183,7 +1183,7 @@ extern "C" [[noreturn]] void TmcLoop(void *)
 			}
 
 			// Wait for the end-of-transfer interrupt
-			timedOut = (TaskBase::Take(TransferTimeout) == 0);
+			timedOut = TaskBase::Take(TransferTimeout);
 			DisableEndOfTransferInterrupt();
 
 #if DEBUG_DRIVER_TIMEOUT
