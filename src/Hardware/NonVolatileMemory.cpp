46922298ba8a08b48d80b15405b5a59301d9f152
David Crocker <dcrocker@eschertech.com>
2020-11-06 12:29:06 +0000

    Introduced memcpy32u

--- src/Hardware/NonVolatileMemory.cpp
+++ src/Hardware/NonVolatileMemory.cpp
@@ -25,7 +25,7 @@ void NonVolatileMemory::EnsureRead() noexcept
 	if (state == NvmState::notRead)
 	{
 #if SAME5x
-		memcpy(&buffer, reinterpret_cast<const void *>(SEEPROM_ADDR), sizeof(buffer));
+		memcpyu32(reinterpret_cast<uint32_t *>(&buffer), reinterpret_cast<const uint32_t *>(SEEPROM_ADDR), sizeof(buffer)/sizeof(uint32_t));
 #elif SAM4E || SAM4S || SAME70
 		const bool cacheEnabled = Cache::Disable();
 		flash_read_user_signature(reinterpret_cast<uint32_t*>(&buffer), sizeof(buffer)/sizeof(uint32_t));
@@ -34,7 +34,7 @@ void NonVolatileMemory::EnsureRead() noexcept
 			Cache::Enable();
 		}
 #elif SAMC21
-		memcpy(&buffer, reinterpret_cast<const void *>(RWW_ADDR), sizeof(buffer));
+		memcpyu32(reinterpret_cast<uint32_t *>(&buffer), reinterpret_cast<const uint32_t *>(RWW_ADDR), sizeof(buffer)/sizeof(uint32_t));
 #else
 # error Unsupported processor
 #endif
@@ -60,7 +60,7 @@ void NonVolatileMemory::EnsureWritten() noexcept
 	{
 		// No need to erase on the SAME5x because the EEPROM emulation manages it
         while (NVMCTRL->SEESTAT.bit.BUSY) { }
-        memcpy(reinterpret_cast<uint8_t*>(SEEPROM_ADDR), &buffer, sizeof(buffer));
+        memcpyu32(reinterpret_cast<uint32_t*>(SEEPROM_ADDR), reinterpret_cast<const uint32_t*>(&buffer), sizeof(buffer)/sizeof(uint32_t));
 		state = NvmState::clean;
         while (NVMCTRL->SEESTAT.bit.BUSY) { }
 	}
