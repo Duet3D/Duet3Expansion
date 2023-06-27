b6fc001e0ffbcfee17fc4d21c0d6436a8bd7c76d
David Crocker <dcrocker@eschertech.com>
2023-06-27 11:46:34 +0100

    Added a static_assert to TaskPriorities.h

--- src/TaskPriorities.h
+++ src/TaskPriorities.h
@@ -11,17 +11,19 @@
 // Task priorities
 namespace TaskPriority
 {
-	static constexpr unsigned int SpinPriority = 1;							// priority for tasks that rarely block
-	static constexpr unsigned int HeatPriority = 2;
-	static constexpr unsigned int TmcOpenLoop = 2;							// priority of the TMC task when in open loop modes
-	static constexpr unsigned int AinPriority = 2;
-	static constexpr unsigned int CanReceiverPriority = 3;
-	static constexpr unsigned int MovePriority = 3;
-	static constexpr unsigned int Accelerometer = 3;
-	static constexpr unsigned int ClosedLoopDataTransmission = 3;
-	static constexpr unsigned int TmcClosedLoop = 4;						// priority of the TMC task when in closed loop mode
-	static constexpr unsigned int CanAsyncSenderPriority = 5;
-	static constexpr unsigned int CanClockPriority = 5;
+	constexpr unsigned int SpinPriority = 1;						// priority for tasks that rarely block
+	constexpr unsigned int HeatPriority = 2;
+	constexpr unsigned int TmcOpenLoop = 2;							// priority of the TMC task when in open loop modes
+	constexpr unsigned int AinPriority = 2;
+	constexpr unsigned int CanReceiverPriority = 3;
+	constexpr unsigned int MovePriority = 3;
+	constexpr unsigned int Accelerometer = 3;
+	constexpr unsigned int ClosedLoopDataTransmission = 3;
+	constexpr unsigned int TmcClosedLoop = 4;						// priority of the TMC task when in closed loop mode
+	constexpr unsigned int CanAsyncSenderPriority = 5;
+	constexpr unsigned int CanClockPriority = 5;
+
+	static_assert(CanClockPriority < configMAX_PRIORITIES);
 }
 
 #endif /* SRC_TASKPRIORITIES_H_ */
