682a8397d8c256b2f21e81078a2c7c4071c589af
David Crocker <dcrocker@eschertech.com>
2023-08-15 13:14:57 +0100

    Read the LDC1612 in the AIN task so that the reading is kept up to date

--- src/CommandProcessing/ScanningSensorHandler.cpp
+++ src/CommandProcessing/ScanningSensorHandler.cpp
@@ -12,18 +12,45 @@
 #include <Hardware/LDC1612.h>
 #include <Platform/Platform.h>
 #include <CanMessageFormats.h>
+#include <AnalogIn.h>
 
 static LDC1612 *sensor = nullptr;
+static AnalogIn::AdcTaskHookFunction *oldHookFunction = nullptr;
+static uint32_t lastReading = 0;
+
+static void LDC1612TaskHook() noexcept
+{
+	// The LDC1612 generates lots of bus errors if we try to read the data when no new data is available
+	if (sensor->IsChannelReady(0))
+	{
+		uint32_t val;
+		if (sensor->GetChannelResult(0, val))		// if no error
+		{
+			lastReading = val;						// save all 28 bits of data + 4 error bits
+		}
+		else
+		{
+			lastReading = 0;
+		}
+	}
+
+	if (oldHookFunction != nullptr)
+	{
+		oldHookFunction();
+	}
+}
 
 void ScanningSensorHandler::Init() noexcept
 {
-#if defined(TOOL1LC)
+#if defined(SAMMMYC21)
+	// Assume we are using a LDC1612 breakout board with its own crystal, so we don't need to generate a clock
+#elif defined(TOOL1LC) || defined(SZP)
 	// Set up the external clock to the LDC1612.
 	// The higher the better, but the maximum is 40MHz. We use the 96MHz DPLL output divided by 3 to get 32MHz.
 	ConfigureGclk(GclkNumPA23, GclkSource::dpll, 3, true);
 	SetPinFunction(LDC1612ClockGenPin, GpioPinFunction::H);
-#elif defined(SAMMMYC21)
-	// Assume we are using a LDC1612 breakout board with its own crystal, so we don't need to generate a clock
+#else
+# error LDC support not implemented for this processor
 #endif
 
 	sensor = new LDC1612(Platform::GetSharedI2C());
@@ -31,11 +58,12 @@ void ScanningSensorHandler::Init() noexcept
 	if (sensor->CheckPresent())
 	{
 		sensor->SetDefaultConfiguration(0);
+		oldHookFunction = AnalogIn::SetTaskHook(LDC1612TaskHook);
 	}
 	else
 	{
 		DeleteObject(sensor);
-#if defined(TOOL1LC)
+#if defined(TOOL1LC) || defined(SZP)
 		ClearPinFunction(LDC1612ClockGenPin);
 #endif
 	}
@@ -48,18 +76,7 @@ bool ScanningSensorHandler::IsPresent() noexcept
 
 uint32_t ScanningSensorHandler::GetReading() noexcept
 {
-	if (sensor != nullptr)
-	{
-		uint32_t val;
-		if (sensor->GetChannelResult(0, val))
-		{
-			if ((val >> 28) == 0)				// if no error
-			{
-				return val;						// return all 28 bits of result
-			}
-		}
-	}
-	return 0;
+	return lastReading;
 }
 
 GCodeResult ScanningSensorHandler::SetOrCalibrateCurrent(uint32_t param, const StringRef& reply, uint8_t& extra) noexcept
