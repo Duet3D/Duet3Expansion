14c085a70218a0b19654c79a921b1054d6bd34d5
David Crocker <dcrocker@eschertech.com>
2024-01-17 17:00:56 +0000

    Fixed SZP build

--- src/Config/SZP.h
+++ src/Config/SZP.h
@@ -92,7 +92,7 @@ constexpr Pin Lis3dhInt1Pin = PortAPin(19);
 #endif
 
 #if SUPPORT_LDC1612
-constexpr uint16_t LDC1612_I2cAddress = 0x2A;				// pin 4 is tied low on this board
+constexpr uint16_t LDC1612_I2CAddress = 0x2A;				// pin 4 is tied low on this board
 constexpr Pin LDC1612ClockGenPin = PortAPin(23);
 constexpr Pin LDC1612InterruptPin = PortAPin(22);
 #endif
