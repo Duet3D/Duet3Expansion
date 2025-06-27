258dad70d5e9327d3b16455f12adaf27f04da61d
David Crocker <dcrocker@eschertech.com>
2025-06-27 15:52:22 +0100

    Fixed issue 1124 on expansion boards

--- src/Movement/StepperDrivers/TMC51xx.cpp
+++ src/Movement/StepperDrivers/TMC51xx.cpp
@@ -949,7 +949,8 @@ void TmcDriverState::SetStallDetectFilter(bool sgFilter) noexcept
 
 void TmcDriverState::SetStallMinimumStepsPerSecond(unsigned int stepsPerSecond) noexcept
 {
-	maxStallStepInterval = StepTimer::StepClockRate/max<unsigned int>(stepsPerSecond, 1u);
+	if (stepsPerSecond == 0) { stepsPerSecond = 1; }					// avoid divide-by-zero errors
+	maxStallStepInterval = StepTimer::StepClockRate/stepsPerSecond;
 	UpdateRegister(WriteTcoolthrs, (GetTmcClockSpeed() + (128 * stepsPerSecond))/(256 * stepsPerSecond));
 }
 
