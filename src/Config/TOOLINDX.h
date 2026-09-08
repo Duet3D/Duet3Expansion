dee7418f44f283ed2b5b3d8a1ef74bf7e713450e
dc42 <dcrocker@duet3d.com>
2026-09-08 12:06:22 +0100

    Corrected accelerometer port name for TOOLINDX

--- src/Config/TOOLINDX.h
+++ src/Config/TOOLINDX.h
@@ -412,7 +412,7 @@ constexpr PinDescription PinTable[] =
 
 	// Virtual pins
 #if SUPPORT_LIS3DH
-	{ TcOutput::none,	TccOutput::none,	AdcInput::none,		SercomIo::none,		SercomIo::none,		Nx,	"i2c.lis3dh,i2c.lis2dw,i2c.accelerometer"	},	// LIS3DH or LIS2DW12 sensor connected via I2C
+	{ TcOutput::none,	TccOutput::none,	AdcInput::none,		SercomIo::none,		SercomIo::none,		Nx,	"i2c.lis,lis3dh,i2c.lis3dsh,i2c.lis2dw"	},	// LIS3DH or LIS2DW12 sensor connected via I2C
 #endif
 #if SUPPORT_LDC1612
 	{ TcOutput::none,	TccOutput::none,	AdcInput::ldc1612,	SercomIo::none,		SercomIo::none,		Nx,	"i2c.ldc1612"	},	// LDC1612 sensor connected via I2C
